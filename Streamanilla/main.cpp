200425d66bad1f28cf99ed3837037eaeac734e02
dim3coder <dmi3coder@gmail.com>
2016-12-17 21:41:57 +0200

    Polling and pushup simple data w/ client

--- Streamanilla/main.cpp
+++ Streamanilla/main.cpp
@@ -11,10 +11,24 @@
 using namespace std;
 
 static void ev_handler(struct mg_connection *nc, int ev, void *p) {
-    if(ev ==MG_EV_ACCEPT){
-            mg_send(nc, &(nc->recv_mbuf), nc->recv_mbuf.len);
+    struct mbuf *io = &nc->recv_mbuf;
+    
+    static int a = 1;
+    switch (ev) {
+        case MG_EV_ACCEPT:
             cout<<"man it's \n";
-    };
+            break;
+        case MG_EV_SEND:
+            cout << "MG_EV_SEND dude,for real! "<< a++ << endl;
+            break;
+        case MG_EV_RECV:
+            // This event handler implements simple TCP echo server
+            mg_send(nc, io->buf, io->len);  // Echo received data back
+            mbuf_remove(io, io->len);      // Discard data from recv buffer
+            break;
+        default:
+            break;
+    }
     
 }
 
@@ -22,7 +36,7 @@ int main(int argc, const char * argv[]) {
     cout << "Hello, there! :>)";
     struct mg_mgr mgr;
     mg_mgr_init(&mgr, NULL);
-    mg_bind(&mgr, "1234",&ev_handler);
+    mg_bind(&mgr, "tcp://192.168.0.100:1234",&ev_handler);
     
     for ( ; ; ) {
         mg_mgr_poll(&mgr, 1000);
